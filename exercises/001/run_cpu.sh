1ca12636a4129f983bac6bdf1054fbadec843b61
schreck2 <schreck2@jrlogin09.jureca>
2026-05-26 10:32:12 +0200

    modified bash files for JURECA

--- exercises/001/run_cpu.sh
+++ exercises/001/run_cpu.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+#SBATCH --account=training2542
+#SBATCH --partition=dc-cpu-devel
+#SBATCH --output=run001.out
+#SBATCH --error=run001.err
+
 export OMP_PROC_BIND=true
 export OMP_PROC_BIND=spread
 
-./build_cpu/hello
+srun ./build_cpu/hello
