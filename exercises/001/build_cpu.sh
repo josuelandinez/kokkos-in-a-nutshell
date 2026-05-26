9359cb5d6482a49460c18cfb297c400e75879cf7
mathilde22 <m.schreck@uni-bonn.de>
2026-05-26 10:40:23 +0200

    Revert "modified bash files for JURECA"
    revert commit

--- exercises/001/build_cpu.sh
+++ exercises/001/build_cpu.sh
@@ -1,21 +1,6 @@
 #!/bin/bash
-#SBATCH --account=training2542
-#SBATCH --partition=dc-cpu-devel
-#SBATCH --output=build.out
-#SBATCH --error=build.err
-
-module --force purge
-module load Stages/2025 \
-            GCC/13.3.0 \
-            OpenMPI/5.0.5 \
-            CUDA/12 \
-            CMake/3.29.3 \
-
 rm -rf build_cpu
 mkdir build_cpu
 #cpu build, openmp
-cmake -B build_cpu \
-  -DCMAKE_BUILD_TYPE=Release \
-  -DKokkos_ENABLE_OPENMP=ON \
-  -DKokkos_ENABLE_SERIAL=ON
+cmake -B build_cpu -DKokkos_ENABLE_OPENMP=ON
 cmake --build build_cpu -j 8 
--- exercises/001/run_cpu.sh
+++ exercises/001/run_cpu.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-#SBATCH --account=training2542
-#SBATCH --partition=dc-cpu-devel
-#SBATCH --output=run001.out
-#SBATCH --error=run001.err
-
 export OMP_PROC_BIND=true
 export OMP_PROC_BIND=spread
 
-srun ./build_cpu/hello
+./build_cpu/hello
